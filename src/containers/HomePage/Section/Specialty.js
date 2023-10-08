a4d3256078a27960ce3190117a44187f5785bb38
duong <nguyenxuanduong1804@gmail.com>
2023-10-08 17:19:48 +0700

    #48 add carousel slide

--- src/containers/HomePage/Section/Specialty.js
+++ src/containers/HomePage/Section/Specialty.js
@@ -3,10 +3,44 @@ import { Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import './Specialty.scss';
 import { FormattedMessage } from 'react-intl';
+
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css"
+// import "slick-carousel/slick/slick-theme.css"
 class Specialty extends Component {
     render() {
+        let settings = {
+            dots: false,
+            infinite: true,
+            speed: 500,
+            slidesToShow: 1,
+            slidesToScroll: 1
+        }
         return (
-            <div>speciality</div>
+            <div className='section-specialty'>
+                <div className='specialty-content'>
+                <Slider {...settings}>
+                    <div>
+                        <h3>1</h3>
+                    </div>
+                    <div>
+                        <h3>2</h3>
+                    </div>
+                    <div>
+                        <h3>3</h3>
+                    </div>
+                    <div>
+                        <h3>4</h3>
+                    </div>
+                    <div>
+                        <h3>5</h3>
+                    </div>
+                    <div>
+                        <h3>6</h3>
+                    </div>
+                </Slider>
+                </div>
+            </div>
         );
     }
 
