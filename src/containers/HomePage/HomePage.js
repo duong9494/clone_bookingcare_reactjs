16e3ceab554d863d8911845b1168657508dd4765
duong <nguyenxuanduong1804@gmail.com>
2023-10-09 21:17:01 +0700

    #50 add section co so y te

--- src/containers/HomePage/HomePage.js
+++ src/containers/HomePage/HomePage.js
@@ -3,15 +3,27 @@ import { Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import HomeHeader from './HomeHeader';
 import Specialty from './Section/Specialty';
+import MedicalFacility from './Section/MedicalFacility'
+import './HomePage.scss'
+
+import "slick-carousel/slick/slick.css"
+import "slick-carousel/slick/slick-theme.css"
 
 class HomePage extends Component {
 
     render() {
+        let settings = {
+            dots: false,
+            infinite: true,
+            speed: 500,
+            slidesToShow: 4,
+            slidesToScroll: 1
+        }
         return (
             <div>
                 <HomeHeader />
-                <Specialty />
-                <div style={{height: '300px'}}></div>
+                <Specialty settings = {settings} />
+                <MedicalFacility settings = {settings} />
             </div>
         );
     }
